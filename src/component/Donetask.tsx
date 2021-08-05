5a9b233ead7392a6dc848f3f1b55c91ec8612a3d
Kaofang <benyapa_kh@cmu.ac.th>
2021-08-05 17:52:44 +0700

    finishDonetask

---
+++ src/component/Donetask.tsx
@@ -0,0 +1,16 @@
+type doneTaskProps = {
+    id: number;
+    name: string;
+  }
+  
+  const Donetask = ({id, name,} : doneTaskProps) => {
+    return (
+        <div
+            className="flex justify-between h-8 items-center py-6 border-b "
+          >
+            <span className="text-2xl line-through"> {name} </span>
+          </div>
+    )
+  }
+  
+  export default Donetask
