54c49557bfe46bf51333908859032147079f6d4e
Kaofang <benyapa_kh@cmu.ac.th>
2021-08-05 17:53:08 +0700

    AddHeader

---
+++ src/component/WebHeader.tsx
@@ -0,0 +1,11 @@
+const WebHeader = () => {
+    return (
+        <div className='flex justify-center items-end space-x-2'>
+        <span className='text-center italic my-2 text-2xl'>Minimal Todo List </span>
+        <span className='text-gray-400 italic my-2 text-xl'>by Benyapa Kiaotai</span>
+      </div>
+
+    )
+  }
+  
+  export default WebHeader
