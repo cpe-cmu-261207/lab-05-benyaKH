1d65852df9148ec40c08456e5ca5c5615d8f60e3
Kaofang <benyapa_kh@cmu.ac.th>
2021-08-05 17:53:31 +0700

    FinishTask

---
+++ src/component/task.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+type TaskProps = {
+    id: number;
+    name: string;
+    doneFn: Function;
+    deleteFn: Function; //Function type
+  }
+
+
+  const Task = ({id, name, doneFn, deleteFn} : TaskProps) => {
+    const [Mouse,setIsMouseInside] = useState<boolean>(false);
+    return (
+        <div
+            className="flex justify-between h-8 items-center py-6 border-b"
+            onMouseEnter={()=> setIsMouseInside(true)} onMouseLeave={()=> setIsMouseInside(false)}
+          >
+            <span className="text-2xl"> {name} </span>
+            <div className="flex space-x-1 items-center" style={(Mouse)? {visibility:"visible"}:{visibility:"hidden"}}>
+              <button className="bg-green-400 w-24 text-2xl" onClick={ () => doneFn(id)}>Done</button>
+              <button className="bg-red-400 w-24 text-2xl" onClick={ () => deleteFn(id) }>Delete</button>
+            </div>
+          </div>
+    )
+  }
+  
+  export default Task
